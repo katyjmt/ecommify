aa1a98130e84e42c94770b554ed0a287218ceec6
katyjmt <43462706+katyjmt@users.noreply.github.com>
2023-08-27 17:16:01 +1000

    Removed tag model from GET all route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,7 +7,7 @@ const { Category, Product } = require('../../models');
 router.get('/', async (req, res) => {
   try {
     const categoryData = await Category.findAll({
-      include: [{ model: Product }, { model: Tag }],
+      include: [{ model: Product }],
     });
     res.status(200).json(categoryData);
   } catch {
